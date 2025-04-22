63f313e59aa2404a95775f99a07cc960362aca32
Julio Canizalez <juliocanizalez@outlook.com>
2025-04-22 15:38:13 -0600

    Add routes to index routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import authRoutes from './auth.routes';
+import projectRoutes from './project.routes';
 
 export default async function registerRoutes(
   server: FastifyInstance,
 ): Promise<void> {
   await server.register(authRoutes, { prefix: '/api/auth' });
+  await server.register(projectRoutes, { prefix: '/api/projects' });
 }
